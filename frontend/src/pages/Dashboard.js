be7d1db950113d23aa1ff5a611c37258b5408111
Deniz Onur Duzgun <59659739+dduzgun-security@users.noreply.github.com>
2021-03-25 13:03:20 -0400

    add useEffect import in dashboard
    Co-authored-by: Alano Terblanche <alanoterblanche@gmail.com>

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
